900f87cd5f91bcf6982c1e1bc7602001784365f1
Aziz Sufyanov <aziz-sufyanov@mail.ru>
2023-03-21 00:15:53 +0300

    Simple forms for authorization and registration
     - Added some beautiful forms
     - Added simple handlers for authorization and registration
     - Added links to authorization and registration on the index page
     - Added the run.bat file to run the application

--- app/flaskr/auth.py
+++ app/flaskr/auth.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-from __main__ import app
+from main import app  # __main__
+from flask import Flask, render_template, request, redirect, url_for
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET'])
 def login():
     return render_template('auth/login.html')
 
@@ -10,3 +10,26 @@ def login():
 @app.route('/signup')
 def signup():
     return render_template('auth/signup.html')
+
+
+@app.route('/login/handler', methods=['POST'])
+def login_handler():
+    print(request.form, flush=True)
+    return redirect(url_for('index'))
+
+
+@app.route('/signup/handler', methods=['POST'])
+def signup_handler():
+    print(request.form, flush=True)
+    return redirect(url_for('index'))
+
+
+@app.route('/user/guest')
+def guest():
+    return '<h1>THIS PAGE IN DEVELOPMENT</h1>'
+
+
+
+@app.route('/base')
+def base():
+    return render_template('base.html', page_name='Login')
